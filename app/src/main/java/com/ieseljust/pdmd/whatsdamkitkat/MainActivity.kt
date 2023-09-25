5cfd90655d2e7242f7cb6141c467e26d97685d21
RaulSales <raulsahe@protonmail.com>
2023-09-25 11:28:05 +0200

    sending the structure

---
+++ app/src/main/java/com/ieseljust/pdmd/whatsdamkitkat/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ieseljust.pdmd.whatsdamkitkat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
