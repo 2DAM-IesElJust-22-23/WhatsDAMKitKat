833e407326fa6c8d5bf8dad02bb02d5edd16990b
ittsraul <raulsahe@protonmail.com>
2023-10-23 10:43:32 +0200

    sending the work before free time

--- app/src/main/java/com/example/ieseljust/whatsdam/repository/MessageRepository.kt
+++ app/src/main/java/com/example/ieseljust/whatsdam/repository/MessageRepository.kt
@@ -1,4 +1,26 @@
 package com.example.ieseljust.whatsdam.repository
 
-class MessageRepository {
+class MessageRepository private constructor(){
+    //singleton is here baby
+    companion object{
+        /***
+         * per a implementar el patró SINGLETON 'sutilitza companion object l'equivalent a static d eJAVA. Despŕes instanciarem el repositori (INSTANCE)
+         * #1: Instanciem la clase
+         *
+         *
+         *
+         * ***/
+        //#1
+        private var INSTANCE: MessageRepository? = null;
+
+        fun getInstance(): MessageRepository{
+            if(INSTANCE == null){
+                INSTANCE = MessageRepository()
+            }
+            return INSTANCE!!
+        }
+
+    }
+
+
 }
--- changelog.md
+++ changelog.md
@@ -1,1 +1,18 @@
 Raul Sales i Antonio :  Els canvis realitzats per a tindre en compte, i serien l'us de intents per a treballar amb el valor que s'ha introduit, de tipus String a el mainActivity, per tal de que la finestra de missatges i puga treballar amb els valors.
+
+
+# Sprint 2.5
+
+Hem refactoritzat l'estructura de MVC o intent de MVC a MVVM afegint un repositori. 
+
+intent de MVC
+|_controller
+|_entity
+|_repository
+
+MVVM
+|_views
+|_viewsmodel
+|_repository
+|_model
+
